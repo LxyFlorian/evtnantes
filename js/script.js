b933a991de54922df128612b9cd0c23606ef3b73
Florian <florianelie8@gmail.com>
2019-10-03 16:22:51 +0200

    add hover tooltip on description

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var url = 'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_agenda-evenements-nantes-nantes-metropole&facet=emetteur&facet=rubrique&facet=lieu&facet=ville&rows=15';
 
+
 var App = new Vue({
 
     el: '#app',
@@ -16,6 +17,7 @@ var App = new Vue({
             axios.get(url)
                 .then(function(response){
                     self.events = response.data.records;
+                    console.log(response);
                 })
                 .catch(function(error){
                     console.log(error);
