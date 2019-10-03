76fa961d42c446cffee226eb1ba6654c8be7cbc5
Florian <florianelie8@gmail.com>
2019-10-03 15:33:28 +0200

    add search for name.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var url = 'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_agenda-evenements-nantes-nantes-metropole&facet=emetteur&facet=rubrique&facet=lieu&facet=ville&rows=15';
 
 var App = new Vue({
+
     el: '#app',
     data: {
         events: null,
+        label: "",
     },
     created: function(){
         this.initialize();
@@ -14,15 +16,36 @@ var App = new Vue({
             axios.get(url)
                 .then(function(response){
                     self.events = response.data.records;
-                    console.log(response.data);
                 })
                 .catch(function(error){
                     console.log(error);
                 })
                 .finally(function (data) {
-                    console.log(data)
                 })
+        }
+
+    },
+    computed: {
+        filteredEvents: function(){
+            var eventsF = this.events,
+                nom = this.label;
+
+            if(!nom){
+                return eventsF;
+            }
+
+            nom = nom.trim().toLowerCase();
+
+            eventsF = eventsF.filter(function(item){
+                if(item.fields.nom.toLowerCase().indexOf(nom) !== -1){
+                    return item;
+                }
+            });
+            // Return an array with the filtered data.
+            return eventsF;
         }
     }
 
+
 });
+
